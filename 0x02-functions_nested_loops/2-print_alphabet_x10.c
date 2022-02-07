4208abe3c36524b39257565835a2d18969c8fb64
FloLys <4440@holbertonschool.com>
2022-02-07 10:28:43 -0800

    try222

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,12 @@ void print_alphabet_x10(void)
 	char alpha;
 	int times;
 
-	for (times = 0; times < 10; x++)
+	for (times = 0; times < 10; times++)
 	{
 		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
-	
+
 		}
 		_putchar('\n');
 	}
