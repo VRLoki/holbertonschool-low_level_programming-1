70ca0a0aa50fff68c008e91572442ae347671f95
FloLys <4440@holbertonschool.com>
2022-02-09 16:48:07 -0800

    1010update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,12 +9,12 @@ void print_triangle(int size)
 	int cn1;
 	int cn2;
 
-	for (cn1 = 0; cn1 < size; cn1++)
+	for (cn1 = 1; cn1 < size; cn1++)
 	{
 		for (cn2 = cn1; cn2 < size; cn2++)
 			_putchar(' ');
 
-		for (cn2 = 0; cn2 <= cn1; cn2++)
+		for (cn2 = 1; cn2 <= cn1; cn2++)
 			_putchar('#');
 
 		_putchar('\n');
