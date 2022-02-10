c6bfa15ec21890ef23afbd7b82ad51a74663709d
FloLys <4440@holbertonschool.com>
2022-02-09 16:36:33 -0800

    10update6

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,20 @@
  */
 void print_triangle(int size)
 {
+	int cn1;
+	int cn2;
+
+	for (cn1 = 0; cn1 < size; cn1++)
+	{
+		for (cn2 = cn1; cn2 < size; cn2++)
+			_putchar(' ');
+
+		for (cn2 = 0; cn2 <= cn1; cn2++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+
 	if (size <= 0)
 		_putchar('\n');
-
-	else
-	{
-		int cn1;
-		int cn2;
-
-		for (cn1 = 1; cn1 < size; cn1++)
-		{
-			for (cn2 = cn1; cn2 < size; cn2++)
-				_putchar(' ');
-
-			for (cn2 = 1; cn2 <= cn1; cn2++)
-				_putchar('#');
-
-			_putchar('\n');
-		}
-	}
 }
