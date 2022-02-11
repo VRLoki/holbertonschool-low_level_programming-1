5c85b00a3b0d19e2c4dd12b705f2bc72d026ea6a
FloLys <4440@holbertonschool.com>
2022-02-11 10:33:08 -0800

    3 upload

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - takes a pointer to an int as parameter and
+ * updates the value it points to 98
+ * @s: variable to show length
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char *s[];
+	int len;
+
+	len = _strlen(s);
+
+	return (len);
+}
