8ae44eab59c39165b11d307ee189ab8ed094e475
FloLys <4440@holbertonschool.com>
2022-02-11 17:40:38 -0800

    3 upload

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * _strlen - Calculate the Length
- * @s: variable to show length
- * Return: 0
+ * _strlen - Calculate the Length of string
+ * @s: string to show length
+ * Return: string length
  */
 int _strlen(char *s)
 {
-	size_t strlen(const char *s);
-	return (strlen(s));
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{}
+
+	return (a);
 }
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - print a string to standard output
+ * @str: string to print to output
+ */
+void _puts(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
