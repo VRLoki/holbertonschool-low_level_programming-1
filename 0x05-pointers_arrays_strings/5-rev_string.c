e7a87c546c1e87952bf7ab6c7bcc867fc17b8ee2
FloLys <4440@holbertonschool.com>
2022-02-12 18:10:57 -0800

    5 upload

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses order of string
+ * @s: string to print
+ */
+void rev_string(char *s)
+{
+	int a, b;
+
+	a = 0;
+
+	while (s[a] != '\0')
+		a++;
+
+	for (b = a - 1; b >= 0; b--)
+
+	s = b;
+}
