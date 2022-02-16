1fe083a7c7b3f9d7229c729944b9e6c05b82ec23
FloLys <4440@holbertonschool.com>
2022-02-16 11:26:20 -0800

    3 corrected missing {}

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,12 +6,13 @@
  * @s1: destination string
  * @s2: source string
  * Return: 0 = equal. Negative = s1 < s2. Positive = s1 > s2.
-*/
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int a;
 
 	for (a = 0; s2[a] == s1[a] && s2[a] != '\0'; a++)
+	{}
 
 	return (s2[a] - s1[a]);
 }
